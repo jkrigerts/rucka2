d69940cf333f4d6e12f94717e372e30bfcecc912
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-11-27 08:46:04 +0200

    HOTFIX OG image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,11 @@
 
 <svelte:head>
 	<title>Ruckas muiža</title>
+  <meta name="theme-color" content="#9e004f" />
   <meta property="og:title" content="Ruckas muiža" />
   <meta property="og:type" content="website" />
   <meta property="og:description" content="2023. gada DECEMBRA PASĀKUMU KALENDĀRS" />
-  <meta property="og:image" content="ruckasmuiza-calendar-og.png" />
+  <meta property="og:image" content="https://www.ruckasmuiza.lv/ruckasmuiza-calendar-og.png/" />
 </svelte:head>
 
 <slot />
