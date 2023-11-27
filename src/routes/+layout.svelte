70f72ecfb0e7a492b03972a85cb3d7fad502cfdb
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2023-11-27 08:37:31 +0200

    Basic OG added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,11 @@
 
 <svelte:head>
 	<title>Ruckas muiža</title>
+  <meta property="og:title" content="Ruckas muiža" />
+  <meta property="og:type" content="website" />
+  <meta property="og:description" content="2023. gada DECEMBRA PASĀKUMU KALENDĀRS" />
+  <meta property="og:image" content="ruckasmuiza-calendar-og.png" />
 </svelte:head>
 
-  <slot />
+<slot />
 
-  <style></style>
