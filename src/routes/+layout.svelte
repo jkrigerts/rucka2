be890e45a628a825cdd29b219fad732f556db159
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2024-03-08 11:22:36 +0200

    DEV added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,11 @@
 </script>
 
 <svelte:head>
-	<title>Ruckas muiža</title>
+	<title>Ruckas muiža DEV</title>
   <meta name="theme-color" content="#9e004f" />
-  <meta property="og:title" content="Ruckas muiža" />
+  <meta property="og:title" content="Ruckas muiža DEV" />
   <meta property="og:type" content="website" />
-  <meta property="og:description" content="Ruckas muiža" />
+  <meta property="og:description" content="Ruckas muiža DEV" />
   <meta property="og:image" content="https://www.ruckasmuiza.lv/ruckasmuiza-calendar-og.jpg/" />
 </svelte:head>
 
